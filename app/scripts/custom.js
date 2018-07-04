4745278122c47339effd2b9d5a59d9442b172153
w.kujawski1 <w.kujawski1@gmail.com>
2018-07-04 21:41:25 +0200

    final changes

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -4,3 +4,22 @@ function cloneBacon() {
     const cloned = imgToClone.cloneNode();
     baconContainer.appendChild(cloned);
 }
+
+function simpleValidation(e) {
+    const form = document.querySelector('.form');
+    const message = form.querySelector('.form__message');
+    const inputs = Array.from(form.querySelectorAll('.form_validate'));
+    const isValid = inputs.every((input) => input.checkValidity());
+    message .classList.toggle('form__message--valid', isValid);
+    message .classList.toggle('form__message--invalid', !isValid);
+    const text = isValid ? 'Pysznie! Wszystko działa' : 'Ojej, coś popsułeś';
+    message.innerHTML = text;
+}
+
+
+function init() {
+    document.querySelector('.baconTrigger').addEventListener('click', cloneBacon);
+    document.querySelector('.form__submit').addEventListener('click', simpleValidation);
+}
+
+document.addEventListener('DOMContentLoaded', init);
